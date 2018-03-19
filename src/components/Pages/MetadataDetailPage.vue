365349bf14620a32b30253562c8a8cceeea41151
Dominik Haas <shaodiese@gmail.com>
2018-03-19 10:48:39 +0100

    Added MetadataDetailPage

--- src/components/Pages/MetadataDetailPage.vue
+++ src/components/Pages/MetadataDetailPage.vue
@@ -1,10 +1,29 @@
 <template>
-  <v-layout column align-center>
-  
+  <v-layout column xs8 align-center>
+
+    <v-flex xs8 fill-height>
+
+      <v-card class="xs8">
+        <v-card-title>Metadata title</v-card-title>
+        <v-chip v-for="i in 5" :key="i">
+          dude {{ i }}
+        </v-chip>
+        
+      </v-card>
+
+      <v-card class="xs8">
+        <v-card-title>Metadata Info</v-card-title>
+        <v-card-text>desc</v-card-text>
+      </v-card>
+
+    </v-flex>
+
     <v-card class="my-3">
       <p>Metadata Detail Page with id: {{ $route.params.id }}</p>
     </v-card>
-  
+
+
+
   </v-layout>
 </template>
 
@@ -12,7 +31,7 @@
   
   export default {
     components: {
-    },
+    }
   }
 </script>
 
