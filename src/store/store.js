05587eabede6a10c7431b8264c923d7ad233affb
Dominik Haas <shaodiese@gmail.com>
2018-04-23 17:13:58 +0200

    Removed comment

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,5 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-/*
-import dataGetters from './getters';
-// import { dataMutations, ADD_METADATA } from './mutations';
-import dataMutations from './mutations';
-import dataActions from './actions';
-*/
 
 /* eslint-disable no-unused-vars */
 import { metadata } from './modules/metadata';
