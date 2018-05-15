d497d8065cb5ab41d30e8a46e345bf587f2394e1
Dominik Haas <shaodiese@gmail.com>
2018-05-15 14:40:29 +0200

    Added Imagemin; Minor changes to the uglify options

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -37,8 +37,13 @@ const webpackConfig = merge(baseWebpackConfig, {
     new UglifyJsPlugin({
       uglifyOptions: {
         compress: {
-          warnings: false
-        }
+          warnings: false,
+          ecma: 6, // 6 or greater to enable transform ES5 code into smaller ES6+ equivalent forms.
+        },
+        mangle: {
+          toplevel: true, // to mangle names declared in the top level scope.
+          safari10: true, // to work around the Safari 10 loop iterator bug "Cannot declare a let variable twice". See also: the safari10 output option.        
+        },
       },
       sourceMap: config.build.productionSourceMap,
       parallel: true
