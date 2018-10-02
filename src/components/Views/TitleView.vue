bd56fad360d6ab069c5d3715a8c33ab8dd10327e
Dominik Haas <shaodiese@gmail.com>
2018-10-02 10:50:55 +0200

    Initial steps for Landingpage rework Slogan

--- src/components/Views/TitleView.vue
+++ src/components/Views/TitleView.vue
@@ -63,7 +63,59 @@
             </v-layout>
           </v-flex>
 
-          <v-flex hidden-sm-and-down pt-5
+          <v-flex pt-5 >
+
+            <v-card>
+
+              <v-img
+                  background-color="primary"
+                >
+                  <!-- v-bind="{['style'] : dynamicCardBackground }"
+                  height="150px" -->
+                
+                <v-container grid-list-xs fill-height px-3 pt-3 pb-0>
+                  <v-layout column>
+                    <v-flex xs12 py-0>
+
+                      <div class="hidden-sm-and-down envidat_slogan display-1">
+                        {{ slogan }}
+                      </div>
+
+                      <div class="hidden-xs-only hidden-md-and-up envidat_slogan headline">
+                        {{ slogan }}
+                      </div>
+
+                      <div class="hidden-sm-and-up envidat_slogan headline">
+                        {{ slogan }}
+                      </div>
+                    </v-flex>
+
+                  </v-layout>
+                </v-container>
+
+
+              </v-img>
+
+              <v-card-title primary-title>
+                      <div>{{ subSlogan }}</div>
+              </v-card-title>
+
+              <!-- <v-card-actions class="ma-0 pa-2"
+                              style="position: absolute; bottom: 0; right: 0;"> -->
+              <v-card-actions >
+                <v-spacer></v-spacer>
+
+                <v-btn @click.native="buttonCallback"
+                        color="primary"
+                        small
+                >
+                  {{ buttonText }}
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+
+          <!-- <v-flex hidden-sm-and-down pt-5
                   class="envidat_slogan display-1">
             {{ slogan }}
           </v-flex>
@@ -76,7 +128,7 @@
           <v-flex hidden-sm-and-up pt-3
                   class="envidat_slogan headline">
             {{ slogan }}
-          </v-flex>
+          </v-flex> -->
 
         </v-layout>
       </v-flex>
@@ -95,6 +147,9 @@
     props: {
       title: String,
       slogan: String,
+      subSlogan: String,
+      buttonText: String,
+      buttonCallback: Function,
     },
     data: () => ({
       // xsLogo,
