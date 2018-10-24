4166e38a9712feff65f502defb0f220dcc1f4dcc
Dominik Haas <shaodiese@gmail.com>
2018-10-24 17:32:49 +0200

    Increased patch version to 0.3.91; added version to browse page;

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
+  VERSION: JSON.stringify(require('../package.json').version),
   ENVIDAT_PROXY: '"/ui/proxy/ServiceProxyServlet?server=0&serverpath="',
   SOLR_PROXY: '"/ui/proxy/ServiceProxyServlet?server=1&serverpath="',
 }
--- config/test.env.js
+++ config/test.env.js
@@ -3,5 +3,8 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"'
+  NODE_ENV: '"testing"',
+  VERSION: JSON.stringify(require('../package.json').version),
+  ENVIDAT_PROXY: '"/ui/proxy/ServiceProxyServlet?server=0&serverpath="',
+  SOLR_PROXY: '"/ui/proxy/ServiceProxyServlet?server=1&serverpath="',
 })
