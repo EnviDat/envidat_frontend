4676207fba94fc405822b9e4fcca5ae9da8ab155
Dominik Haas <shaodiese@gmail.com>
2019-06-06 16:13:16 +0200

    Added genericTextLayout with markdown; Added Policies text

--- src/components/Pages/PoliciesPage.vue
+++ src/components/Pages/PoliciesPage.vue
@@ -6,11 +6,11 @@
 
     <v-layout row wrap >
 
-      <!-- <v-flex xs12 md8 offset-md2 lg10 offset-lg1>
+      <v-flex xs12 lg10 offset-lg1>
 
-        <img-and-text-layout :dark="true"
+        <img-and-text-layout :dark="false"
                         :blur="true"
-                        :img="missionImg"
+                        :img="books"
                         :height="300"
                         :textFontSize="16"
                         :parallax="true"
@@ -22,28 +22,16 @@
                                   :outlined="true"
                                   color="white"
                                   iconColor="white"
-                                  toolTipText ="Close About Page"
+                                  toolTipText ="Close Policies Page"
                                   :toolTipBottom="true"
                                   v-on:clicked="catchBackClicked" />
 
-          </img-and-text-layout>
-      </v-flex> -->
+        </img-and-text-layout>
+      </v-flex>
 
-      <v-flex xs12 md8 offset-md2 lg10 offset-lg1 px-3 mt-5 
-      >
-        <v-container grid-list-lg pa-0>
-          <v-layout row wrap >
 
-            <v-flex my-2
-                    v-for="(info, index) in $store.policiesInfo" :key="index" >
-
-              <h1>{{ info.title }}</h1>
-
-              <text>{{ info.text }}</text>
-
-            </v-flex>
-          </v-layout>
-        </v-container>
+      <v-flex xs12 md8 offset-md2 lg10 offset-lg1 mt-5 >
+        <generic-text-layout :textArray="policiesInfo" />
       </v-flex>
 
     </v-layout>
@@ -63,22 +51,14 @@
     SET_APP_BACKGROUND,
     SET_CURRENT_PAGE,
   } from '@/store/mutationsConsts';
+
   import BaseIconButton from '@/components/BaseElements/BaseIconButton';
-
   import ImgAndTextLayout from '@/components/Layouts/ImgAndTextLayout';
+  import GenericTextLayout from '@/components/Layouts/GenericTextLayout';
 
-  // import team from '@/assets/about/team.jpg';
-  // import teamSmall from '@/assets/about/team_small.jpg';
-  import mission from '@/assets/about/mission.jpg';
-  import missionSmall from '@/assets/about/mission_small.jpg';
-  // import handsSmall from '@/assets/about/hands_small.jpg';
-  // import orga from '@/assets/about/EnviDat_organigram.png';
-  // import orgaSmall from '@/assets/about/EnviDat_organigram_small.png';
-
-  // import conceptSmall from '@/assets/about/concept_small.jpg';
-  // import communitySmall from '@/assets/about/community_small.jpg';
-  // import wslLogo from '@/assets/about/wslLogo.jpg';
-  // import wslLogoSmall from '@/assets/about/wslLogo_small.jpg';
+  import books from '@/assets/policies/books.jpg';
+  import inkpen from '@/assets/policies/inkpen.jpg';
+  import lawBooks from '@/assets/policies/law_books.jpg';
 
   // import ExpandableCard from '@/components/Cards/ExpandableCard';
 
@@ -98,76 +78,22 @@
     },
     computed: {
       ...mapGetters({
-        aboutPageBackRoute: 'metadata/aboutPageBackRoute',
+        policiesPageBackRoute: 'policies/policiesPageBackRoute',
+        policiesTitle: 'policies/policiesTitle',
+        policiesInfo: 'policies/policiesInfo',
       }),
       /**
-       * @returns teamImage based on the screen size
+       * @returns missionImage based on the screen size
        */
-      teamImg: function teamImg() {
+      missionImg: function missionImg() {
         if (this.$vuetify.breakpoint.mdAndUp) {
           return team;
         }
 
         return teamSmall;
       },
-      /**
-       * @returns missionImage based on the screen size
-       */
-      missionImg: function teamImg() {
-        if (this.$vuetify.breakpoint.mdAndUp) {
-          return mission;
-        }
-
-        return missionSmall;
-      },
     },
     methods: {
-      /**
-       * @returns the infos of the about page topics. consiting of
-       * - title
-       * - text
-       * - img
-       * - widthClass
-       */
-      // AboutCardInfos: function AboutCardInfos() {
-      //   return [
-      //     {
-      //       title: 'Our Mission',
-      //       text: 'EnviDat is the environmental data portal and repository developed by the Swiss Federal Research Institute WSL. We have the capability to integrate, host and publish data sets. We make environmental monitoring and research data accessible. <p><a href="https://www.wsl.ch/en/about-wsl/programmes-and-initiatives/envidat.html" target="_blank" onclick="event.stopPropagation();" >More about EnviDat as Program of WSL</a></p>',
-      //       img: this.handsSmall,
-      //       widthClass: 'xs12 sm6 md4',
-      //     },
-      //     {
-      //       title: 'Concept',
-      //       text: 'EnviDat supports the user-friendly registration, documentation, storage, publication, search and retrieval of data sets from the environmental domain. We provide various services to our users and we follow a set of principles as summarized in our concept image below. Additional detailed information can be found in our <a href="https://www.dora.lib4ri.ch/wsl/islandora/object/wsl:18703" target="_blank" onclick="event.stopPropagation();" >concept paper on DORA</a>.',
-      //       img: this.conceptSmall,
-      //       widthClass: 'xs12 sm6 md4',
-      //     },
-      //     {
-      //       title: 'Community',
-      //       text: 'With EnviDat, WSL aims to disseminate its data sets as broadly as possible in order to foster international research cooperation in the field of environmental science and contribute to the ongoing cultural evolution in research towards openness, shared data and opportunities for collaboration. Consequently, we are registered in <a href="https://fairsharing.org/biodbcore-001178/" target="_blank" onclick="event.stopPropagation();" >FAIRsharing.org</a> and <a href="https://www.re3data.org/repository/r3d100012587" target="_blank" onclick="event.stopPropagation();" >re3data.org</a> and a contributor community to <a href="http://geoportal.org/community/envidat-community" target="_blank" onclick="event.stopPropagation();" >ESA Geoportal </a>, <a href="https://gcmd.nasa.gov/search/Titles.do?AutoDisplayTitles=true&subset=envidat#titles" target="_blank" onclick="event.stopPropagation();" >NASA GCMD</a> and <a href="http://b2find.eudat.eu/dataset?groups=envidat" target="_blank" onclick="event.stopPropagation();" >EOSC-Hub via B2FIND</a>. ',
-      //       img: this.communitySmall,
-      //       widthClass: 'xs12 sm6 md4',
-      //     },
-      //     {
-      //       title: 'WSL',
-      //       text: 'The Swiss Federal Institute for Forest, Snow and Landscape Research is concerned with the use, development and protection of natural and urban spaces. The focus of our research is on solving problems to do with the responsible use of landscapes and forests and a prudent approach to natural hazards, especially those common in mountainous countries. WSL occupies a leading position internationally in these research areas. We also provide groundwork for sustainable environmental policies in Switzerland. <p><a href="https://www.wsl.ch" target="_blank" onclick="event.stopPropagation();" >For more information have a look at the Website of WSL</a></p>',
-      //       img: this.wslLogoSmall,
-      //       widthClass: 'xs12 sm6 md4',
-      //     },
-      //     {
-      //       title: 'Team',
-      //       text: `<img src="${this.orga} style="width: 100%; height: 100%;" />`,
-      //       img: this.teamSmall,
-      //       widthClass: 'xs12 sm12 md8',
-      //     },
-      //     // {
-      //     //   title: 'Organigram',
-      //     //   text: '<img src="' + this.orga + '" style="width: 100%; height: 100%;" />',
-      //     //   img: this.orgaSmall,
-      //     // },
-      //   ];
-      // },
       /**
        * @description changes the url to page the user was before. Fallback: BrowsePage
        */
@@ -190,20 +116,12 @@
     },
     data: () => ({
       PageBGImage: './app_b_browsepage.jpg',
-      // policiesTitle: 'EnviDat General Policies',
-      // team,
-      // teamSmall,
-      mission,
-      missionSmall,
-      // handsSmall,
-      // conceptSmall,
-      // communitySmall,
-      // wslLogo,
-      // wslLogoSmall,
-      // orga,
-      // orgaSmall,
+      books,
+      inkpen,
+      lawBooks,
     }),
     components: {
+      GenericTextLayout,
       ImgAndTextLayout,
       BaseIconButton,
     },
--- src/store/modules/policies/policies.js
+++ src/store/modules/policies/policies.js
@@ -1,25 +1,72 @@
 
-export const policies = {
-  namespaced: true,
+// use markdown syntax to style the text: https://markdown-it.github.io/
+const policiesState = {
+  policiesPageBackRoute: '',
+  policiesTitle: 'EnviDat General Policies v. 1.0',
   policiesInfo: [
-    { 
-      title: 'Headline',
-      text: 'FOREST FUNCTION DRINKING WATER FOREST FUNCTION GAME RESERVE FOREST FUNCTION LANDSCAPE PROTECTION FOREST FUNCTION MILITARY FOREST FUNCTION NATURE PROTECTION FOREST FUNCTION PROTECTION AGAINST NATURAL HAZARDS',
-      bulletTitle: 'List title',
-      bulletpoints: ['point 1', 'point2', 'point3'],
+    {
+      title: '## Preamble',
+      text: 'The EnviDat policies are subject to change by EnviDat at any time and without notice, other than through posting the updated policies on the www.envidat.ch website. For any questions about the EnviDat general policies, please contact EnviDat support at envidat@wsl.ch.',
     },
     {
-      title: 'Headline 2',
-      text: 'FOREST FUNCTION DRINKING WATER FOREST FUNCTION GAME RESERVE FOREST FUNCTION LANDSCAPE PROTECTION FOREST FUNCTION MILITARY FOREST FUNCTION NATURE PROTECTION FOREST FUNCTION PROTECTION AGAINST NATURAL HAZARDS',
+      title: '## EnviDat Privacy Policy',
+      text: '**(for information collected from users)**',
+      // text: 'FOREST FUNCTION DRINKING WATER FOREST FUNCTION GAME RESERVE FOREST FUNCTION LANDSCAPE PROTECTION FOREST FUNCTION MILITARY FOREST FUNCTION NATURE PROTECTION FOREST FUNCTION PROTECTION AGAINST NATURAL HAZARDS',
+      // bulletTitle: 'EnviDat Privacy Policy',
+      bulletpoints: [
+        '1. EnviDat does not track, collect or retain personal information from users, except as otherwise provided herein.',
+        '2. Depositors gain access with a passwordless login procedure, which ensures that there is no need to store user passwords.',
+        '3. Non-personal information such as IP addresses and cookies may be tracked and retained in order to monitor traffic and aggregated to provide various usage statistics as requested by the depositors. These usage statistics may also be made public.',
+        '4. Server log files containing non-personal information such as IP addresses and requested URLs may be retained. These server logs may be shared with third parties for the purpose of security auditing.',
+        '5. EnviDat will take all reasonable measures to protect the privacy of its users and to resist service interruptions, intentional attacks, or other events that may compromise the security of the EnviDat website.',
+      ],
     },
     {
-      title: 'Headline 3',
-      text: 'FOREST FUNCTION DRINKING WATER FOREST FUNCTION GAME RESERVE FOREST FUNCTION LANDSCAPE PROTECTION FOREST FUNCTION MILITARY FOREST FUNCTION NATURE PROTECTION FOREST FUNCTION PROTECTION AGAINST NATURAL HAZARDS',
-      bulletTitle: 'List title',
-      bulletpoints: ['point 1', 'point2', 'point3'],
+      title: '## EnviDat Metadata Policy',
+      text: '**(for information describing content items in the repository)**' +
+      ' ' +
+      '1. All metadata is in English.' +
+      '2. Access to the metadata is open and free of charge.' +
+      '3. All information is provided “as-is”, EnviDat disclaims all warranties, express or implied.' +
+      '4. The validity, authenticity and quality of the content of submissions is the sole responsibility of the depositor.' +
+      '5. The metadata may be re-used in any medium for any purposes without prior permission, given:' +
+      '  the terms of the Open Data Commons Open Database License (OdBL) are respected, and' +
+      '  a link to the original metadata record is provided.',
+      // bulletpoints: [
+      //   '1. All metadata is in English.',
+      //   '2. Access to the metadata is open and free of charge.',
+      //   '3. All information is provided “as-is”, EnviDat disclaims all warranties, express or implied.',
+      //   '4. The validity, authenticity and quality of the content of submissions is the sole responsibility of the depositor.',
+      //   '5. The metadata may be re-used in any medium for any purposes without prior permission, given:',
+      //   // subpoints: [
+      //   //   'the terms of the Open Data Commons Open Database License (OdBL) are respected, and',
+      //   //   'a link to the original metadata record is provided.',
+      //   // ],
+      // ],
+    },
+    {
+      title: '## EnviDat Metadata Policy',
+      text: `**(for information describing content items in the repository)**
+
+
+        - 1. All metadata is in English. 
+        - 2. Access to the metadata is open and free of charge. 
+
+        * 3. All information is provided “as-is”, EnviDat disclaims all warranties, express or implied. 
+        * 4. The validity, authenticity and quality of the content of submissions is the sole responsibility of the depositor. 
+        * 5. The metadata may be re-used in any medium for any purposes without prior permission, given: 
+            the terms of the Open Data Commons Open Database License (OdBL) are respected, and 
+            a link to the original metadata record is provided.`,
     },
   ],
+};
+
+export const policies = {
+  namespaced: true,
+  state: policiesState,
   getters: {
     policiesInfo: state => state.policiesInfo,
+    policiesTitle: state => state.policiesTitle,
+    policiesPageBackRoute: state => state.policiesPageBackRoute,
   },
 };
