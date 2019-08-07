26eb46cd17d09aa9f4c1a0649e4cd698eede295e
Dominik Haas <shaodiese@gmail.com>
2019-08-07 15:42:52 +0200

    commented WIP Navbar for xs-screens; Changed 'browse data' to 'explore data'

--- src/components/Pages/LandingPage.vue
+++ src/components/Pages/LandingPage.vue
@@ -152,7 +152,7 @@ export default {
     envidatSlogan: 'Environmental Research Data<br/>at your Fingertips',
     // envidatSlogan: 'Environmental Research Data at your Fingertips. Provided by the many research units of the Swiss Federal Institute for Forest, Snow and Landscape WSL.',
     envidatSubSlogan: 'The data is being provided by the many research units of the Swiss Federal Institute for Forest, Snow and Landscape WSL.',
-    sloganButtonText: 'BROWSE DATA',
+    sloganButtonText: 'EXPLORE DATA',
     loginInfos: {
       titleText: 'Do you create data?',
       loginText: 'Creator Login',
