b4037521fd6968aa2c7ac376f2c2adeeb34cff35
Dominik Haas <shaodiese@gmail.com>
2019-08-14 14:44:29 +0200

    Added route and basic template for ProjectPage

--- src/store/projectsMutationsConsts.js
+++ src/store/projectsMutationsConsts.js
@@ -1,3 +1,6 @@
 export const GET_PROJECTS = 'GET_PROJECTS';
 export const GET_PROJECTS_SUCCESS = 'GET_PROJECTS_SUCCESS';
 export const GET_PROJECTS_ERROR = 'GET_PROJECTS_ERROR';
+
+export const PROJECTS_NAMESPACE = 'projects';
+export const PROJECTS_PAGE = 'projectsPage';
