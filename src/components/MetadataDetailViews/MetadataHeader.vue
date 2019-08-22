02a3927f32909ef2f6d76e2d2478f5783aba220d
Dominik Haas <shaodiese@gmail.com>
2019-08-22 15:32:17 +0200

    Added blur to the metadataHeader; lots of code formatting

--- src/components/MetadataDetailViews/MetadataHeader.vue
+++ src/components/MetadataDetailViews/MetadataHeader.vue
@@ -4,10 +4,13 @@
               'pa-3': $vuetify.breakpoint.xsOnly,
             }"
             :dark="dark"
-            color="primary"
-            v-bind="{['style'] : dynamicCardBackground }" >
+            :color="(showPlaceholder || (!showPlaceholder && !metadataTitle)) ? 'primary' : 'white'" >
 
-      <base-icon-button style="position: absolute; top: 0px; right: 0px;"
+      <div v-bind="{['style'] : dynamicCardBackground }" >
+        <!-- this loads the background image -->
+      </div>
+
+      <base-icon-button style="position: absolute; top: 0px; right: 0px; z-index: 2;"
                         material-icon-name="close"
                         icon-color="primary"
                         :color="(showPlaceholder || (!showPlaceholder && !metadataTitle)) ? 'white' : 'primary'"
@@ -16,162 +19,151 @@
                         :tool-tip-bottom="true"
                         @clicked="catchBackClicked" />
 
-      <!--h1 class="py-3" >{{ metadataTitle }} id: {{ $route.params.id }}</h1-->
-      <div v-if="metadataTitle"
-            class="headerTitle py-3"
-            :class="{ 'display-2': $vuetify.breakpoint.lgAndUp,
-                      'display-1': $vuetify.breakpoint.mdAndDown,
-                      'headline': $vuetify.breakpoint.smAndDown,
-            }" >
-        {{ metadataTitle }}
-      </div>
+      <v-layout row wrap
+                :style="`position: relative; z-index: 1;`">
 
-      <div v-if="!metadataTitle && showPlaceholder"
-            class="skeleton skeleton-size-big skeleton-color-concrete skeleton-animation-shimmer" >
-        <div class="bone bone-type-multiline bone-style-steps" />
-      </div>
-
-      <v-divider :dark="dark"
-                :class="{ 'my-1': $vuetify.breakpoint.xsOnly,
-                          'my-2': $vuetify.breakpoint.smAndUp }" />
-
-      <v-layout v-if="authors"
-                row wrap
-      >
-        <tag-chip-author
-          v-for="author in authors"
-          :key="author.name"
-          :name="author.name.trim()"
-          :tool-tip-text="authorToolTipText"
-          @clicked="catchAuthorClicked($event, author.name.trim())"
-        />
-      </v-layout>
-
-      <v-layout v-if="!authors && showPlaceholder"
-                row wrap
-      >
-        <tag-chip-placeholder
-          v-for="n in 5"
-          :key="n"
-          class="headerTag"
-        />
-      </v-layout>
-
-      <v-divider
-        :dark="dark"
-        :class="{ 'my-1': $vuetify.breakpoint.xsOnly,
-                  'my-2': $vuetify.breakpoint.smAndUp }"
-      />
-
-      <v-layout row wrap >
-        <v-flex
-          xs12 sm6 md6 lg3
-          py-0
-          class="headerInfo"
-        >
-          <base-icon-label-view
-            :text="contactName"
-            :icon="contactIcon"
-            icon-tooltip="Main contact"
-            :align-left="true"
-          />
+        <v-flex v-if="metadataTitle"
+                xs12 >
+          <div class="headerTitle py-3"
+                :class="{ 'display-2': $vuetify.breakpoint.lgAndUp,
+                          'display-1': $vuetify.breakpoint.mdAndDown,
+                          'headline': $vuetify.breakpoint.smAndDown,
+                }" >
+            {{ metadataTitle }}
+          </div>
         </v-flex>
 
-        <v-flex xs12 sm6 md6 lg3
-                py-0
-                class="headerInfo"
-        >
-          <base-icon-label-view
-            :text="contactEmail"
-            :icon="mailIcon"
-            icon-tooltip="Email adress of the main contact"
-            :align-left="true"
-            :word-break="true"
-          />
+        <v-flex v-if="!metadataTitle && showPlaceholder"
+                xs12 >
+          <div class="skeleton skeleton-size-big skeleton-color-concrete skeleton-animation-shimmer" >
+            <div class="bone bone-type-multiline bone-style-steps" />
+          </div>
         </v-flex>
 
-        <v-flex xs12 sm6 md6 lg3
-                py-0
-                class="headerInfo"
-        >
-          <base-icon-label-view
-            :text="doi"
-            :icon="doiIcon"
-            icon-tooltip="Data Object Identifier"
-            :align-left="true"
-            :word-break="true"
-          />
+        <v-flex xs12 >
+          <v-divider :dark="dark"
+                    :class="{ 'my-1': $vuetify.breakpoint.xsOnly,
+                              'my-2': $vuetify.breakpoint.smAndUp }" />
         </v-flex>
 
-        <v-flex xs12 sm6 md6 lg3
-                py-0
-                class="headerInfo"
-        >
-          <base-icon-label-view
-            :text="license"
-            :icon="licenseIcon"
-            icon-tooltip="License for Datafiles"
-            :align-left="true"
-          />
+        <v-flex v-if="authors"
+                xs12 >
+          <v-layout row wrap >
+            <tag-chip-author
+              v-for="author in authors"
+              :key="author.name"
+              :name="author.name.trim()"
+              :tool-tip-text="authorToolTipText"
+              @clicked="catchAuthorClicked($event, author.name.trim())" />
+          </v-layout>
         </v-flex>
+
+
+        <v-flex v-if="!authors && showPlaceholder"
+                xs12 >
+          <v-layout row wrap >
+            <tag-chip-placeholder v-for="n in 5"
+                                  :key="n"
+                                  class="headerTag" />
+          </v-layout>
+        </v-flex>
+
+        <v-flex xs12 >
+          <v-divider :dark="dark"
+                    :class="{ 'my-1': $vuetify.breakpoint.xsOnly,
+                              'my-2': $vuetify.breakpoint.smAndUp }" />
+        </v-flex>
+
+        <v-flex xs12 >
+          <v-layout row wrap >
+            <v-flex xs12 sm6 md6 lg3
+                    py-0
+                    class="headerInfo" >
+              <base-icon-label-view :text="contactName"
+                                    :icon="contactIcon"
+                                    icon-tooltip="Main contact"
+                                    :align-left="true" />
+            </v-flex>
+
+            <v-flex xs12 sm6 md6 lg3
+                    py-0
+                    class="headerInfo" >
+              <base-icon-label-view :text="contactEmail"
+                                    :icon="mailIcon"
+                                    icon-tooltip="Email adress of the main contact"
+                                    :align-left="true"
+                                    :word-break="true" />
+            </v-flex>
+
+            <v-flex xs12 sm6 md6 lg3
+                    py-0
+                    class="headerInfo" >
+              <base-icon-label-view :text="doi"
+                                    :icon="doiIcon"
+                                    icon-tooltip="Data Object Identifier"
+                                    :align-left="true"
+                                    :word-break="true" />
+            </v-flex>
+
+            <v-flex xs12 sm6 md6 lg3
+                    py-0
+                    class="headerInfo" >
+              <base-icon-label-view :text="license"
+                                    :icon="licenseIcon"
+                                    icon-tooltip="License for Datafiles"
+                                    :align-left="true" />
+            </v-flex>
+          </v-layout>
+        </v-flex>
+
+        <v-flex xs12 >
+          <v-divider :dark="dark"
+                    :class="{ 'my-1': $vuetify.breakpoint.xsOnly,
+                              'my-2': $vuetify.breakpoint.smAndUp }" />
+        </v-flex>
+
+        <v-flex v-if="tags"
+                xs12 >
+          <v-layout row wrap >
+            <tag-chip v-for="tag in slicedTags"
+                      :key="tag.name"
+                      :name="tag.name"
+                      :selectable="true"
+                      class="headerTag"
+                      @clicked="catchTagClicked($event, tag.name)" />
+
+            <v-flex v-if="tags && maxTagsReached && !showTagsExpanded"
+                    xs2 >
+              <tag-chip class="headerTag"
+                        :name="'...'"
+                        @click.native="showTagsExpanded = !showTagsExpanded" />
+            </v-flex>
+          </v-layout>
+        </v-flex>
+
+        <v-flex v-if="!tags && showPlaceholder"
+                xs12 >
+          <v-layout row wrap >
+            <tag-chip-placeholder v-for="n in 5"
+                                  :key="n"
+                                  class="headerTag" />
+          </v-layout>
+        </v-flex>
+
       </v-layout>
 
-      <v-divider
-        :dark="dark"
-        :class="{ 'my-1': $vuetify.breakpoint.xsOnly,
-                  'my-2': $vuetify.breakpoint.smAndUp }"
-      />
-
-      <v-layout v-if="tags"
-                row wrap
-      >
-        <tag-chip
-          v-for="tag in slicedTags"
-          :key="tag.name"
-          :name="tag.name"
-          :selectable="true"
-          class="headerTag"
-          @clicked="catchTagClicked($event, tag.name)"
-        />
-
-        <v-flex
-          v-if="tags && maxTagsReached && !showTagsExpanded"
-          xs2
-        >
-          <tag-chip
-            class="headerTag"
-            :name="'...'"
-            @click.native="showTagsExpanded = !showTagsExpanded"
-          />
-        </v-flex>
-      </v-layout>
-
-      <v-layout v-if="!tags && showPlaceholder"
-                row wrap
-      >
-        <tag-chip-placeholder
-          v-for="n in 5"
-          :key="n"
-          class="headerTag"
-        />
-      </v-layout>
-
-      <v-card-actions
-        v-if="maxTagsReached"
-        style="position: absolute; bottom: 0px; right: 0px;"
-      >
-        <base-icon-button
-          material-icon-name="expand_more"
-          :outlined="true"
-          color="primary"
-          icon-color="accent"
-          :is-toggled="showTagsExpanded"
-          :rotate-on-click="true"
-          :rotate-toggle="showTagsExpanded"
-          :tool-tip-text="showTagsExpanded ? 'Hide all tags' : 'Show all tags'"
-          :tool-tip-bottom="true"
-          @clicked="showTagsExpanded = !showTagsExpanded"
-        />
+      <v-card-actions v-if="maxTagsReached"
+                      style="position: absolute; bottom: 0px; right: 0px;" >
+        <base-icon-button material-icon-name="expand_more"
+                          :outlined="true"
+                          color="primary"
+                          icon-color="accent"
+                          :is-toggled="showTagsExpanded"
+                          :rotate-on-click="true"
+                          :rotate-toggle="showTagsExpanded"
+                          :tool-tip-text="showTagsExpanded ? 'Hide all tags' : 'Show all tags'"
+                          :tool-tip-bottom="true"
+                          @clicked="showTagsExpanded = !showTagsExpanded" />
       </v-card-actions>
     </v-card>
 </template>
@@ -233,13 +225,19 @@ export default {
     dynamicCardBackground: function dynamicCardBackground() {
       const gradient = this.dark ? this.blackTopToBottom : this.whiteTopToBottom;
 
+      let style = `position: absolute; top: 0px; right: 0px;
+                    height: 100%; width: 100%;
+                    background-image: linear-gradient(0deg, ${gradient});
+                    background-position: center, center; background-size: cover;
+                    background-repeat: initial;
+                    z-index: 0;`;
+
       if (this.titleImg) {
-        return `background-image: linear-gradient(0deg, ${gradient}), url(${this.titleImg});
-        background-position: center, center; background-size: cover;
-        background-repeat: initial;`;
+        style += `background-image: linear-gradient(0deg, ${gradient}), url(${this.titleImg});
+        filter: blur(2px);`;
       }
 
-      return '';
+      return style;
     },
   },
   methods: {
