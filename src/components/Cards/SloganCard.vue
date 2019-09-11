51e47f77fa4c01cd426d14b68f61abfabc2e0cb2
davidhanimann <davidhanimann@gmail.com>
2019-09-11 10:27:56 +0200

    Refactored SloganCard

--- src/components/Cards/SloganCard.vue
+++ src/components/Cards/SloganCard.vue
@@ -1,11 +1,7 @@
 <template>
   <v-card ripple hover @click.native="onClick">
     <v-container grid-list-md pa-0>
-      <v-layout
-        v-bind="{ 'row' : $vuetify.breakpoint.xsOnly ? false : true,
-                  'column' : $vuetify.breakpoint.xsOnly ? true : false,
-        }"
-      >
+      <v-layout :column="$vuetify.breakpoint.xsOnly">
         <v-flex xs5 py-0>
           <v-img
             :src="fingertipsImg"
@@ -14,19 +10,18 @@
         </v-flex>
 
         <v-flex xs7 pa-2>
-          <div
-            class="hidden-sm-and-down envidatSlogan display-1"
-          >{{slogan}}</div>
-
-          <div
-            class="hidden-xs-only hidden-md-and-up envidatSlogan headline px-2"
-          >{{slogan}}</div>
-
+          <div class="hidden-sm-and-down envidatSlogan display-1">
+            {{slogan}}
+          </div>
+          <div class="hidden-xs-only hidden-md-and-up envidatSlogan headline px-2">
+            {{slogan}}
+          </div>
           <div
             class="hidden-sm-and-up envidatSlogan headline x"
             style="text-align: center;"
-          >{{slogan}}</div>
-
+          >
+            {{slogan}}
+          </div>
           <div
             class="pt-3 pb-5"
             :class="{'px-2': $vuetify.breakpoint.smAndDown }"
@@ -38,12 +33,8 @@
       </v-layout>
     </v-container>
 
-
     <v-card-actions class="ma-0 pa-2" style="position: absolute; bottom: 0; right: 0;">
-      <base-rectangle-button
-        :button-text="buttonText"
-        :is-small="true"
-      />
+      <base-rectangle-button :button-text="buttonText" :is-small="true"/>
     </v-card-actions>
   </v-card>
 </template>
@@ -54,15 +45,16 @@ import fingertipsImg from '@/assets/cards/fingertips_small.jpg';
 import BaseRectangleButton from '@/components/BaseElements/BaseRectangleButton';
 
 export default {
+  name: 'SloganCard',
   components: { BaseRectangleButton },
+  data: () => ({
+    fingertipsImg,
+  }),
   props: {
     slogan: String,
     subSlogan: String,
     buttonText: String,
   },
-  data: () => ({
-    fingertipsImg,
-  }),
   methods: {
     onClick() {
       this.$emit('click');
