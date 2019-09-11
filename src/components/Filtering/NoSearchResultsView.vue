587a2cc51b4adb1922cabd8b0edbabd758db72de
davidhanimann <davidhanimann@gmail.com>
2019-09-11 10:11:41 +0200

    Refactoring of SloganCard (this commit is required for previous commit to work, unfortunately)

--- src/components/Filtering/NoSearchResultsView.vue
+++ src/components/Filtering/NoSearchResultsView.vue
@@ -1,8 +1,6 @@
 <template>
   <v-layout column>
-    <v-flex
-      xs12
-      py-4
+    <v-flex xs12 py-4
       :style="`font-weight: 700; background-color: ${$vuetify.theme.errorHighlight}`"
     >
       <h2>{{ noResultText }}</h2>
@@ -12,16 +10,9 @@
       <h3>{{ suggestionText }}</h3>
     </v-flex>
 
-    <v-flex xs12
-      pt-2
-    >
-      <v-container
-        fluid
-        grid-list-md
-        pa-0
-      >
-        <v-layout row wrap
-        >
+    <v-flex xs12 pt-2>
+      <v-container fluid grid-list-md pa-0>
+        <v-layout row wrap>
           <v-flex
             v-for="card in categorycards"
             :key="card.title"
