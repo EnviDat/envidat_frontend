89b91b17884d7beede32ed5a15ca06e97b9b051a
Dominik Haas <shaodiese@gmail.com>
2019-11-06 08:23:16 +0100

    changed singlequotes (for eslint);

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,15 +1,15 @@
-import Vue from "vue";
-import { configure, addDecorator } from "@storybook/vue";
-import "@/plugins/vuetify";
-import Vuetify, { VApp, VContainer, VLayout, VFlex } from "vuetify/lib";
+import Vue from 'vue';
+import { configure, addDecorator, addParameters } from '@storybook/vue';
+import '@/plugins/vuetify';
+import Vuetify, { VApp, VContainer, VLayout, VFlex } from 'vuetify/lib';
 
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
-import globalMethods from "@/components/globalMethods";
-import metaDataFactory from "@/components/metaDataFactory";
+import globalMethods from '@/components/globalMethods';
+import metaDataFactory from '@/components/metaDataFactory';
 
 const cardImages = getCardImages(globalMethods);
-import metadataCards from "@/stories/metadata";
+import metadataCards from '@/stories/metadata';
 
 metadataCards.forEach(element => {
   element.tags = metaDataFactory.convertTags(element.tags, true);
@@ -17,65 +17,73 @@ metadataCards.forEach(element => {
 
 globalMethods.methods.mixinMethods_enhanceMetadatas(metadataCards, cardImages);
 
-import { configureViewport, INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+import { configureViewport, INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 const vuetifyViewports = {
   VuetifyLg: {
-    name: "Vuetify LG",
+    name: 'Vuetify LG',
     styles: {
-      width: "1904px"
+      width: '1904px',
+      height: '100%',
     },
-    type: "desktop"
+    type: 'desktop'
   },
   VuetifyXs: {
-    name: "Vuetify XS",
+    name: 'Vuetify XS',
     styles: {
-      width: "600px"
+      width: '600px',
+      height: '100%',
     },
-    type: "mobile"
+    type: 'mobile'
   },
   VuetifySm: {
-    name: "Vuetify SM",
+    name: 'Vuetify SM',
     styles: {
-      width: "960px"
+      width: '960px',
+      height: '100%',
     },
-    type: "mobile"
+    type: 'mobile'
   },
   VuetifyMd: {
-    name: "Vuetify MD",
+    name: 'Vuetify MD',
     styles: {
-      width: "1264px"
+      width: '1264px',
+      height: '100%',
     },
-    type: "tablet"
+    type: 'tablet'
   },
   VuetifyXl: {
-    name: "Vuetify XL",
+    name: 'Vuetify XL',
     styles: {
-      width: "4096px"
+      width: '4096px',
+      height: '100%',
     },
-    type: "desktop"
+    type: 'desktop'
   }
 };
 
-configureViewport({
-  defaultViewport: "VuetifyMd",
-  viewports: {
-    ...vuetifyViewports,
-    ...INITIAL_VIEWPORTS
-  }
+// configureViewport({
+addParameters({
+  viewport : {
+    // defaultViewport: 'VuetifyMd',
+    viewports: {
+      // ...vuetifyViewports,
+      ...INITIAL_VIEWPORTS
+    },
+  },
 });
 
 
-Vue.component("v-app", VApp);
-Vue.component("v-container", VContainer);
-Vue.component("v-layout", VLayout);
-Vue.component("v-flex", VFlex);
+Vue.component('v-app', VApp);
+Vue.component('v-container', VContainer);
+Vue.component('v-layout', VLayout);
+Vue.component('v-flex', VFlex);
 
 // Ensures every story is wrapped in a v-app tag
 addDecorator(() => ({
-  // template: "<v-app><story/></v-app>"
+  // template: '<v-app><story/></v-app>'
   template: `<v-app>
-    <v-container fluid>
+    <v-container fluid pa-2 grid-list-xs>
       <v-layout row wrap>
         <v-flex>
           <story/>
@@ -85,7 +93,7 @@ addDecorator(() => ({
     </v-app>`
 }));
 
-const req = require.context("@/stories", true, /\.stories.js$/);
+const req = require.context('@/stories', true, /\.stories.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
@@ -94,21 +102,21 @@ function loadStories() {
 configure(loadStories, module);
 
 /* eslint-disable import/no-extraneous-dependencies */
-// import Vue from "vue";
-// import { configure } from "@storybook/vue";
-// import Vuetify, { VApp } from "vuetify/lib";
+// import Vue from 'vue';
+// import { configure } from '@storybook/vue';
+// import Vuetify, { VApp } from 'vuetify/lib';
 
-// // import "vuetify/src/stylus/app.styl";
-// import "material-design-icons-iconfont/dist/material-design-icons.css";
-// import "vuetify/dist/vuetify.min.css";
+// // import 'vuetify/src/stylus/app.styl';
+// import 'material-design-icons-iconfont/dist/material-design-icons.css';
+// import 'vuetify/dist/vuetify.min.css';
 
 // Vue.use(Vuetify, {
-//   iconfont: "md"
+//   iconfont: 'md'
 // });
 
-// Vue.component("v-app", VApp);
+// Vue.component('v-app', VApp);
 
-// const req = require.context("../../src/stories", true, /.stories\.js$/);
+// const req = require.context('../../src/stories', true, /.stories\.js$/);
 
 // function loadStories() {
 //   req.keys().forEach(filename => req(filename));
@@ -121,25 +129,25 @@ function getCardImages(globalMethods) {
   let cardImages = {};
 
   // alert(
-  //   "globalMethods " +
+  //   'globalMethods ' +
   //     typeof globalMethods +
-  //     " globalMethods.methods.mixinMethods_importImages " +
+  //     ' globalMethods.methods.mixinMethods_importImages ' +
   //     typeof globalMethods.methods.mixinMethods_importImages
   // );
 
-  let imgPaths = require.context("@/assets/cards/landscape/", false, /\.jpg$/);
+  let imgPaths = require.context('@/assets/cards/landscape/', false, /\.jpg$/);
   cardImages.landscape = globalMethods.methods.mixinMethods_importImages(imgPaths);
 
-  imgPaths = require.context("@/assets/cards/forest/", false, /\.jpg$/);
+  imgPaths = require.context('@/assets/cards/forest/', false, /\.jpg$/);
   cardImages.forest = globalMethods.methods.mixinMethods_importImages(imgPaths);
 
-  imgPaths = require.context("@/assets/cards/snow/", false, /\.jpg$/);
+  imgPaths = require.context('@/assets/cards/snow/', false, /\.jpg$/);
   cardImages.snow = globalMethods.methods.mixinMethods_importImages(imgPaths);
 
-  imgPaths = require.context("@/assets/cards/diversity/", false, /\.jpg$/);
+  imgPaths = require.context('@/assets/cards/diversity/', false, /\.jpg$/);
   cardImages.diversity = globalMethods.methods.mixinMethods_importImages(imgPaths);
 
-  imgPaths = require.context("@/assets/cards/hazard/", false, /\.jpg$/);
+  imgPaths = require.context('@/assets/cards/hazard/', false, /\.jpg$/);
   cardImages.hazard = globalMethods.methods.mixinMethods_importImages(imgPaths);
 
   return cardImages;
