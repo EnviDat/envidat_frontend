63bc31aca1d317881abc8c9e771780a89d577e49
Dominik Haas <shaodiese@gmail.com>
2019-11-08 13:49:24 +0100

    Added countTags(); Added getPopluarTags for swissfl tags;
    Encapsulated: creation of tags;

--- src/factories/metadataFilterMethods.js
+++ src/factories/metadataFilterMethods.js
@@ -11,6 +11,53 @@
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE.txt', which is part of this source code package.
  */
+const defaultTagOptions = { enabled: true, color: '#e0e0e0', count: 0 };
+
+export function createTag(name, options = defaultTagOptions) {
+
+  let enabled = options.enabled !== undefined ? options.enabled : defaultTagOptions.enabled;
+  let color = options.color ? options.color : defaultTagOptions.color;
+  let count = options.count ? options.count : defaultTagOptions.count;
+
+  if (options.tag) {
+    enabled = options.enabled === undefined && options.tag.enabled !== undefined ? options.tag.enabled : enabled;
+    color = options.color === undefined && options.tag.color ? options.tag.color : color;
+    count = options.count === undefined && options.tag.count ? options.tag.count : count;
+  }
+
+  return { name, enabled, color, count };
+}
+
+/**
+ * Goes through all the tags and checks if they are part of the content list.
+ * @param {array} tags 
+ * @param {array} content 
+ */
+export function getEnabledTags(tags, content) {
+  const updatedTags = [];
+
+  for (let i = 0; i < tags.length; i++) {
+    const tag = tags[i];
+    let found = false;
+
+    for (let j = 0; j < content.length; j++) {
+      const el = content[j];
+
+      if (el.tags && el.tags.length > 0) {
+        const index = el.tags.findIndex(obj => obj.name.includes(tag.name));
+
+        if (index >= 0) {
+          found = true;
+          break;
+        }
+      }
+    }
+
+    updatedTags.push(createTag(tag.name, {enabled: found, color: tag.color, count: tag.count }));
+  }
+
+  return updatedTags;
+}
 
 export function tagsIncludedInSelectedTags(tags, selectedTagNames) {
   let selectedTagFound = 0;
@@ -31,36 +78,48 @@ export function tagsIncludedInSelectedTags(tags, selectedTagNames) {
   return selectedTagFound === selectedTagNames.length;
 }
 
-export function getPopularTags(tags, minimalCount = 5) {
+export function countTags(datasets) {
+  if (!datasets || datasets.length <= 0) return [];
+
   const tagMap = new Map();
 
-  for (let j = 0; j < tags.length; j++) {
-    const tag = tags[j];
+  for (let i = 0; i < datasets.length; i++) {
+    const dataset = datasets[i];
 
-    if (tagMap.has(tag.name)) {
-      tagMap.set(tag.name, tagMap.get(tag.name) + 1);
-    } else {
-      tagMap.set(tag.name, 1);
-    }
+    for (let j = 0; j < dataset.tags.length; j++) {
+      const tag = dataset.tags[j];
 
-  }
+      let count = 1;
+      let existingTag = tagMap.get(tag.name);
 
-  const tagNames = Objects.keys(tagMap);
-  const tagCounts = Object.values(tagMap);
+      if (existingTag) {
+        count += existingTag.count;
+      }
 
-  for (let i = 0; i < tagNames.length; i++) {
-    const name = tagNames[i];
-
-    if (tagCounts[name] < minimalCount) {
-      tagMap.delete(name);
+      tagMap.set(tag.name, createTag(tag.name, { tag: existingTag, count: count }));
     }
   }
 
-  Array.sort(tagNames, (a, b) => {
-    const countA = tagCounts[tagNames.findIndex(a)];
-    const countB = tagCounts[tagNames.findIndex(b)];
-    return countA > countB;
-  })
+  const tagCounts = Array.from(tagMap.values());
 
-  return tagNames;
+  tagCounts.sort((a, b) => { return b.count - a.count; });
+
+  return tagCounts;
+}
+
+export function getPopularTags(datasets, excludeTag, minCount = 5, maxCount = 0) {
+  if (!datasets || datasets.length <= 0) return [];
+
+  const tagCounted = countTags(datasets);
+  const cleandAndCounted = [];
+
+  for (let i = 0; i < tagCounted.length; i++) {
+    const tag = tagCounted[i];
+    if ((tag.count >= minCount)
+      && (excludeTag && tag.name.toLowerCase() !== excludeTag.toLowerCase())
+      && (maxCount > 0 && tag.count < maxCount)) {
+      cleandAndCounted.push(tag);
+    }    
+  }
+  return cleandAndCounted;
 }
