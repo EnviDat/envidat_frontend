34785301559d36d8582ff852c9c20cc701b9c5f3
Dominik Haas <shaodiese@gmail.com>
2019-11-20 16:15:14 +0100

    bugfix: scroll to top click in projectsDetailPage

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,7 @@
                     fill-height
                     v-on:scroll="updateScroll()"
                     ref="appContainer"
-                    :style="currentPageIsBrowsePage ? '' : 'height: calc(100vh - 36px); overflow-y: auto;'" >
+                    :style="currentPageIsBrowsePage ? '' : 'height: calc(100vh - 36px); overflow-y: auto; scroll-behavior: smooth;'" >
         <v-layout column >
           <v-flex xs12 mx-0 >
 
@@ -85,7 +85,7 @@
  * @author Dominik Haas-Artho
  *
  * Created at     : 2019-10-23 16:12:30
- * Last modified  : 2019-11-15 15:50:59
+ * Last modified  : 2019-11-20 16:06:15
  *
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE.txt', which is part of this source code package.
