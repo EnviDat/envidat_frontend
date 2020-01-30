87c9e93533c1ec6d163ce1e75f70d0a0722ba6fc
Dominik Haas <shaodiese@gmail.com>
2020-01-30 09:09:57 +0100

    change MetadataDetails so that the form is more compact;

--- src/components/Metadata/MetadataDetails.vue
+++ src/components/Metadata/MetadataDetails.vue
@@ -7,12 +7,14 @@
     <v-card-text v-if="details && details.length > 0">
       <v-form>
         <div v-for="val in details"
-              :key="val.label" >
+              :key="val.label"
+              class="mt-2" >
           <v-text-field v-if="isSingleText(val.text)"
                         :id="val.label"
                         :label="val.label"
                         :name="val.label"
                         :value="val.text"
+                        hide-details
                         readonly />
 
           <v-textarea v-if="!isSingleText(val.text)"
@@ -20,6 +22,7 @@
                       :label="val.label"
                       :name="val.label"
                       :value="val.text"
+                      hide-details
                       readonly />
         </div>
       </v-form>
