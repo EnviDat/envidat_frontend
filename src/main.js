808694642ba3b75868d9790c1fc9c7e2be2f9fa6
Dominik <shaodiese@gmail.com>
2020-02-13 15:26:29 +0100

    fixed: lint errors

--- src/main.js
+++ src/main.js
@@ -12,8 +12,6 @@
  * file 'LICENSE.txt', which is part of this source code package.
  */
 
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'babel-polyfill';
 import Vue from 'vue';
 
