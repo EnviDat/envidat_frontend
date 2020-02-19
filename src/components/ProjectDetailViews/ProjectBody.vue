a3604508f27f7474d1a7a6453619e4eb229193fd
Dominik Haas <shaodiese@gmail.com>
2020-02-19 11:28:14 +0100

    replaced body with expandableTextLayout

--- src/components/ProjectDetailViews/ProjectBody.vue
+++ src/components/ProjectDetailViews/ProjectBody.vue
@@ -1,45 +1,10 @@
 <template>
-  <v-card :dark="dark" >
+  <expandable-text-layout title="Description"
+                            :text="description"
+                            :maxTextLength="maxTextLength"
+                            :emptyText="emptyText"
+                            :showPlaceholder="showPlaceholder" />
 
-    <v-card-title class="bodyTitle title">Description</v-card-title>
-
-    <v-card-text v-if="fullDescription"
-                  ref="description"
-                  class="heightAndScroll pb-4"
-                  v-html="markdownText" >
-
-    </v-card-text>
-
-    <v-card-text v-if="showPlaceholder && !fullDescription" >
-      <div class="skeleton skeleton-size-normal skeleton-color-concrete skeleton-animation-shimmer">
-        <div class="bone bone-type-multiline bone-style-paragraph" />
-      </div>
-    </v-card-text>
-
-    <v-card-text v-if="!showPlaceholder && !fullDescription"
-                  style="color: red;" >
-      {{ emptyText }}
-    </v-card-text>
-
-
-    <v-card-actions v-if="maxDescriptionLengthReached"
-                    class="ma-0 pa-2"
-                    style="position: absolute; bottom: 5px; right: 0px;" >
-
-      <base-icon-button class="mr-2"
-                        material-icon-name="expand_more"
-                        icon-color="accent"
-                        color="accent"
-                        outlined
-                        :rotateOnClick="true"
-                        :rotateToggle="showFullDescription"
-                        :tooltipText="showFullDescription ? 'Hide full description' : 'Show full description'"
-                        @clicked="showFullDescription = !showFullDescription" />
-
-    </v-card-actions>
-
-
-  </v-card>
 </template>
 
 <script>
@@ -56,58 +21,28 @@
  * file 'LICENSE.txt', which is part of this source code package.
 */
 
-import remark from 'remark';
-import html from 'remark-html';
-import BaseIconButton from '@/components/BaseElements/BaseIconButton';
+import ExpandableTextLayout from '@/components/Layouts/ExpandableTextLayout';
 
 export default {
+  name: 'ProjectBody',
   components: {
-    BaseIconButton,
+    ExpandableTextLayout,
   },
   props: {
     description: String,
-    dark: Boolean,
     showPlaceholder: Boolean,
   },
   computed: {
-    markdownText() {
-      return remark().use(html).processSync(this.fullDescription);
-    },
-    fullDescription() {
-      if (this.description) {
-        if (this.maxDescriptionLengthReached) {
-          return this.showFullDescription ? this.description.trim() : `${this.description.trim().substring(0, this.maxTextLength)}...`;
-        }
-
-        return this.description.trim();
-      }
-
-      return '';
-    },
-    maxDescriptionLengthReached() {
-      return this.description && this.description.length > this.maxTextLength;
-    },
   },
   methods: {
   },
   data: () => ({
-    showFullDescription: false,
-    maxTextLength: 2000,
+    maxTextLength: 1500,
     emptyText: 'No description found for this project.',
   }),
 };
 </script>
 
 <style scoped>
-
-  .bodyTitle {
-    font-family: 'Libre Baskerville', serif !important;
-    font-weight: 700 !important;
-  }
-
-  .heightAndScroll {
-    max-height: 500px;
-    overflow-y: auto !important;
-  }
 
 </style>
