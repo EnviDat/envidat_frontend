cd47473055eea3009829d6107232f6608d8bd87a
hanimann <davidhanimann@gmail.com>
2020-06-10 14:01:37 +0200

    Replace german with english text

--- src/components/Metadata/MetadataLocationCesium.vue
+++ src/components/Metadata/MetadataLocationCesium.vue
@@ -5,12 +5,12 @@
         <zoom-btn @zoomIn="zoomIn" @zoomOut="zoomOut" @zoomToGeometry="zoomToGeometry" />
       </transition>
     </div>
-    <div style="position: absolute; z-index: 9999; bottom: 40px;">
+    <div style="position: absolute; z-index: 1000; bottom: 40px;">
       <slot></slot>
     </div>
     <div id="credits">
       <a style="padding-right: 10px;" href="https://cesium.com/cesiumjs/" target="_blank">CesiumJS</a>
-      <a href="https://www.bing.com/maps/" target="_blank">Bilder &copy; Bing Maps</a>
+      <a href="https://www.bing.com/maps/" target="_blank">Images &copy; Bing Maps</a>
     </div>
   </div>
 </template>
