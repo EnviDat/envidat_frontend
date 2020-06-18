c30de0c83d9ec140fc30eaaf1ee832d5aec3a29b
hanimann <davidhanimann@gmail.com>
2020-06-18 13:45:55 +0200

    Remount Map2 after window resize

--- src/components/Metadata/MetadataGeo2.vue
+++ src/components/Metadata/MetadataGeo2.vue
@@ -2,86 +2,90 @@
   <v-card v-bind="mapSize">
     <v-card-title class="title metadata_title">Location Geoservices</v-card-title>
 
-    <v-card-text v-if="configFile" style="width: 100%; height: 100%; max-height: 100%;">
-        <Map2 :config="configFile" :map-div-id="'map-small'">
-      <v-btn fab small color="primary" @click.native.stop="openFullscreen">
-        <v-icon medium style="height:auto;">fullscreen</v-icon>
-      </v-btn>
-        </Map2>
+    <v-card-text v-if="configFile" style="width: 100%; height: 100%; position: relative;">
+      <Map2 :config="configFile" :map-div-id="'map-small'">
+        <v-btn fab small color="primary" @click.native.stop="openFullscreen">
+          <v-icon medium style="height:auto;">fullscreen</v-icon>
+        </v-btn>
+      </Map2>
     </v-card-text>
 
   </v-card>
 </template>
 
 <script>
-import axios from 'axios';
-import Map2 from './Map2';
+  import axios from 'axios';
+  import Map2 from './Map2';
 
-export default {
-  name: 'MetadataGeo2',
-  components: { Map2 },
-  props: {
-    genericProps: Object,
-  },
-  data: () => ({
-    map: null,
-    mapLayer: null,
-    configFile: null,
-    selectedLayer: null,
-    smallSize: 300,
-    mediumSize: 500,
-    largeSize: 725,
-    fullWidthSize: 875,
-  }),
-  computed: {
-    ready() {
-      if (this.genericProps.config) { return true; }
-       return false;
+  export default {
+    name: 'MetadataGeo2',
+    components: { Map2 },
+    props: {
+      genericProps: Object,
     },
-    title() {
-      return this.mixinMethods_getGenericProp('title');
-    },
-    mapSize() {
-      let width = this.largeSize;
-      let height = this.mediumSize;
+    data: () => ({
+      map: null,
+      mapLayer: null,
+      configFile: null,
+      selectedLayer: null,
+      smallSize: 300,
+      mediumSize: 500,
+      largeSize: 725,
+      fullWidthSize: 875,
+    }),
+    computed: {
+      ready() {
+        return !!this.genericProps.config;
+      },
+      title() {
+        return this.mixinMethods_getGenericProp('title');
+      },
+      mapSize() {
+        let width = this.largeSize;
+        let height = this.mediumSize;
 
-      if (this.$vuetify.breakpoint.xsOnly) {
-        width = this.mediumSize;
-        height = this.smallSize;
-      } else if (this.$vuetify.breakpoint.smAndDown) {
-        width = this.fullWidthSize;
-        height = this.smallSize;
-      } else if (this.$vuetify.breakpoint.mdAndDown) {
-        width = this.fullWidthSize;
-      }
+        if (this.$vuetify.breakpoint.xsOnly) {
+          width = this.mediumSize;
+          height = this.smallSize;
+        } else if (this.$vuetify.breakpoint.smAndDown) {
+          width = this.fullWidthSize;
+          height = this.smallSize;
+        } else if (this.$vuetify.breakpoint.mdAndDown) {
+          width = this.fullWidthSize;
+        }
 
-      return {
-        style: `width: ${width}px !important;
+        return {
+          style: `width: ${width}px !important;
                   max-width: 100%;
                   height: ${height}px !important;`,
-      };
+        };
+      },
     },
-  },
-  watch: {
-    ready() {
-      this.loadConfig();
+    watch: {
+      ready: {
+        handler() {
+          if (this.genericProps.config) {
+            this.loadConfig();
+          }
+        },
+        immediate: true,
+      },
     },
-  },
-  methods: {
-    openFullscreen() {
-      this.$router.push({ path: '/metadata/dataset-for-testing-geoservices/map' });
+    methods: {
+      openFullscreen() {
+        this.$router.push({ path: '/metadata/dataset-for-testing-geoservices/map' });
+      },
+      loadConfig() {
+        const url = this.genericProps.config.url;
+        axios.get(url)
+          .then((res) => {
+            this.configFile = res.data;
+            this.selectedLayer = this.configFile.layers.find(layer => layer.visibility).name;
+            return 'Success';
+          });
+      },
     },
-    loadConfig() {
-      const url = this.genericProps.config.url;
-      axios.get(url)
-        .then((res) => {
-          this.configFile = res.data;
-          this.selectedLayer = this.configFile.layers.find(layer => layer.visibility).name;
-          return 'Success';
-        });
-    },
-  },
-};
+  };
 </script>
 <style scoped>
   .layers {
@@ -91,6 +95,7 @@ export default {
     padding: 3px;
     z-index: 9999;
   }
+
   .selected {
     background-color: cadetblue;
   }
