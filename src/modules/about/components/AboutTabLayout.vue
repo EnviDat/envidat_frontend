c6c5c3af334efa573e049151e555a4de0619a347
Dominik Haas <shaodiese@gmail.com>
2020-06-26 11:55:51 +0200

    added AboutTabLayout for better small screen structering

---
+++ src/modules/about/components/AboutTabLayout.vue
@@ -0,0 +1,56 @@
+<template>
+
+  <v-row no-gutters
+          :class="{'px-4': $vuetify.breakpoint.mdAndUp,
+                  'px-3': $vuetify.breakpoint.sm }" >
+
+    <v-col class="pt-3" cols="12" >
+      <img-and-text-layout :img="titleImage"
+                            :height="$vuetify.breakpoint.smAndDown ? 100 : 150"
+                            :title="title" />
+    </v-col>
+
+    <v-col v-show="loading"
+            class="pt-5" 
+            cols="12">
+      {{ loadingText }}
+    </v-col>
+
+    <v-col v-if="!loading && markdownContent"
+            cols="12"
+            class="pt-5"
+            v-html="markdownContent">
+
+    </v-col>
+
+  </v-row>
+
+</template>
+
+<script>
+/**
+ * @summary about tab layout for the different markdown tabs
+ * @author Dominik Haas-Artho
+ *
+ * Created at     : 2019-10-23 16:12:30
+ * Last modified  : 2019-11-20 13:39:14
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+import ImgAndTextLayout from '@/components/Layouts/ImgAndTextLayout';
+
+export default {
+  name: 'AboutTabLayout',
+  props: {
+    title: String,
+    titleImage: String,
+    loading: Boolean,
+    loadingText: String,
+    markdownContent: String,
+  },
+  components: {
+    ImgAndTextLayout,
+  },
+};
+</script>
