305890879a8a9b85ca992b23f4eba4dc2f8dccad
Dominik Haas <shaodiese@gmail.com>
2020-07-16 17:02:35 +0200

    fleshed out the UserAvatar copmonent with Gravatar and nameInitials

--- src/components/Layouts/UserAvatar.vue
+++ src/components/Layouts/UserAvatar.vue
@@ -1,45 +1,71 @@
 <template>
-      <base-icon-button materialIconName="account_circle"
-                        tooltipBottom
-                        color="highlight"
-                        iconColor="black"
-                        isSmall />
+  <v-avatar color="secondary"
+            :size="size" >
+
+    <img v-if="emailHash"
+          :src="`https://gravatar.com/avatar/${emailHash}?s=${size}&d=${defaultGavatar}&r=g`" />
+
+    <span v-if="!emailHash && nameInitials"
+          class="white--text"
+          :class="initialsTextClass" >{{ nameInitials }}</span>
+
+    <v-icon v-if="!emailHash && !nameInitials"
+            color="black"
+            :small="size <= 20"
+            :large="size > 40 && size < 128"
+            :x-large="size >= 128" >account_circle</v-icon>
+
+  </v-avatar>
 </template>
 
 <script>
 /**
  * UserAvatar.vue
  *
- * @summary
+ * @summary Shows the users avatar circle
  * @author Dominik Haas-Artho
  *
  * Created at     : 2019-10-23 14:11:27
- * Last modified  : 2019-10-31 15:29:14
+ * Last modified  : 2020-07-16 16:54:10
  *
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE.txt', which is part of this source code package.
 */
-import BaseIconButton from '@/components/BaseElements/BaseIconButton';
 
 export default {
   components: {
-    BaseIconButton,
   },
   props: {
-    compact: Boolean,
     clickCallback: Function,
+    nameInitials: String,
+    emailHash: String,
+    defaultGavatar: {
+      type: String,
+      default: 'identicon',
+    },
+    size: {
+      type: Number,
+      default: 48,
+    },
   },
   data: () => ({
-    modeInfoPrefix: 'Special View',
-    tooltipText: 'You are in a specific view which shows data for',
   }),
   computed: {
+    initialsTextClass() {
+      if (this.size >= 128) {
+        return 'display-2';
+      } 
+
+      if (this.size > 48) {
+        return 'display-1';
+      } 
+
+      if (this.size <= 32) {
+        return 'subtitle-1';
+      }
+
+      return 'headline';
+    },
   },
 };
 </script>
-
-<style scoped>
-  .item {
-    margin: 0px 5px;
-  }
-</style>
