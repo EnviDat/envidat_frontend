7096b5163d3daa2791abd3c51e4c35e3ba5a9272
Dominik Haas <shaodiese@gmail.com>
2020-08-13 08:59:00 +0200

    added dashboardpage

---
+++ src/modules/user/components/DashboardPage.vue
@@ -0,0 +1,110 @@
+
+<template >
+
+    <signinView :prefilledEmail="prefilledEmail"
+                :prefilledKey="prefilledKey"
+                :signInLoading="signInLoading"
+                :signInSuccess="signInSuccess"
+                :signedIn="user !== null"
+                :signedInColor="$vuetify.theme.themes.light.highlight"
+                :signedInEmail="user ? user.email : null"
+                :requestLoading="requestLoading"
+                :requestSuccess="requestSuccess"
+                :formErrorText="errorText()"
+                :errorFieldText="error"
+                :showError="error !== null"
+                :errorField="errorField"
+                :errorColor="$vuetify.theme.themes.light.errorHighlight"
+                @requestToken="catchRequestToken"
+                @signIn="catchSignIn"
+                @signOut="catchSignOut" />
+
+</template>
+
+<script>
+/**
+ * Dashboard Page
+ *
+ * @summary Dashboard page
+ * @author Dominik Haas-Artho
+ *
+ * Created at     : 2020-07-14 14:18:32 
+ * Last modified  : 2020-08-12 15:53:55
+ */
+
+import { mapGetters } from 'vuex';
+
+import {
+  USER_NAMESPACE,
+  GET_USER_CONTEXT,
+  USER_SIGNIN,
+  REQUEST_TOKEN,
+  VALIDATION_ERROR,
+  USER_SIGNOUT,
+} from '@/modules/user/store/userMutationsConsts';
+
+import SigninView from './SigninView';
+
+export default {
+  components: {
+    SigninView,
+  },
+  beforeMount() {
+    this.checkUserSignedIn();
+  },
+  computed: {
+    ...mapGetters(
+      USER_NAMESPACE,
+      [
+        'userLoading',
+        'signInLoading',
+        'signInSuccess',
+        'requestLoading',
+        'requestSuccess',
+        'user',
+        'error',
+        'errorType',
+        'errorField',
+      ],
+    ),
+    prefilledEmail() {
+      return this.$route.query.email;
+    },
+    prefilledKey() {
+      return this.$route.query.key;
+    },
+  },
+  methods: {
+    errorText() {
+      let errMsg = 'Please make sure everything is filled correctly';
+
+      if (this.error) {
+        if (this.errorType === VALIDATION_ERROR) {
+          errMsg = `A field was filled incorrectly: ${this.error}`;
+        }
+      }
+
+      return errMsg;
+    },
+    checkUserSignedIn() {
+      this.$store.dispatch(`${USER_NAMESPACE}/${GET_USER_CONTEXT}`);
+    },
+    catchSignIn(email, key) {
+      this.$store.dispatch(`${USER_NAMESPACE}/${USER_SIGNIN}`, { email, key });
+    },
+    catchRequestToken(email) {
+      this.$store.dispatch(`${USER_NAMESPACE}/${REQUEST_TOKEN}`, { email });
+    },
+    catchSignOut() {
+      this.$store.dispatch(`${USER_NAMESPACE}/${USER_SIGNOUT}`);
+    },
+  },
+  data: () => ({
+  }),
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
--- src/modules/user/routes.js
+++ src/modules/user/routes.js
@@ -5,7 +5,7 @@
  * @author Dominik Haas-Artho
  *
  * Created at     : 2020-07-14 14:20:05 
- * Last modified  : 2020-07-14 18:19:27
+ * Last modified  : 2020-08-12 16:11:24
  * 
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE.txt', which is part of this source code package.
@@ -14,6 +14,8 @@
 import {
   USER_SIGNIN_PATH,
   USER_SIGNIN_PAGENAME,
+  USER_DASHBOARD_PATH,
+  USER_DASHBOARD_PAGENAME,
 } from '@/router/routeConsts';
 
 export const userRoutes = [
@@ -21,5 +23,10 @@ export const userRoutes = [
     path: USER_SIGNIN_PATH,
     name: USER_SIGNIN_PAGENAME,
     component: () => import(/* webpackPrefetch: true, webpackChunkName: "signinPage" */ '@/modules/user/components/SigninPage'),
-  },  
+  },
+  {
+    path: USER_DASHBOARD_PATH,
+    name: USER_DASHBOARD_PAGENAME,
+    component: () => import(/* webpackPrefetch: true, webpackChunkName: "dashboardPage" */ '@/modules/user/components/DashboardPage'),
+  },
 ];
