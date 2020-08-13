17786c1c4689def6691bc3df049dcb4df68d23a2
Dominik <shaodiese@gmail.com>
2020-08-13 14:00:52 +0200

    added basic photoCredits json

---
+++ src/store/photoCredits.js
@@ -0,0 +1,18 @@
+export const photoCredits = [
+  {
+    title: 'EnviDat Homepage background',
+    author: 'Chris Holgersson',
+    image: 'app_b_landingpage.jpg',
+    creditHtml: '<span>Photo by <a href="https://unsplash.com/@chrisholgersson?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Chris Holgersson</a> on <a href="https://unsplash.com/collections/1979213/views?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
+  },
+  {
+    title: 'User not found',
+    author: 'Maarten van den Heuvel',
+    creditHtml: '<span>Photo by <a href="https://unsplash.com/@mvdheuvel?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Maarten van den Heuvel</a> on <a href="https://unsplash.com/collections/225019/follow-me?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
+  },
+  {
+    title: 'User not found 2',
+    author: 'Freddy Marschall',
+    creditHtml: '<span>Photo by <a href="https://unsplash.com/@freddymarschall?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Freddy Marschall</a> on <a href="https://unsplash.com/collections/225019/follow-me?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>',
+  },  
+];
