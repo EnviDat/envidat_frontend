317ea3820567790533c53604f17edf7de7019fc9
Dominik Haas <shaodiese@gmail.com>
2020-08-19 16:52:14 +0200

    improved variable naming

--- src/modules/metadata/store/metadataActions.js
+++ src/modules/metadata/store/metadataActions.js
@@ -5,7 +5,7 @@
  * @author Dominik Haas-Artho
  *
  * Created at     : 2019-10-23 16:34:51
- * Last modified  : 2019-11-28 13:53:56
+ * Last modified  : 2020-08-19 11:08:09
  *
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE.txt', which is part of this source code package.
@@ -68,8 +68,8 @@ function contentFilterAccessibility(value) {
 }
 
 
-function contentFilteredByTags(value, selectedTagNames) {
-  if (value.tags && tagsIncludedInSelectedTags(value.tags, selectedTagNames)) {
+function contentFilteredByTags(entry, selectedTagNames) {
+  if (entry.tags && tagsIncludedInSelectedTags(entry.tags, selectedTagNames)) {
     return true;
   }
 
