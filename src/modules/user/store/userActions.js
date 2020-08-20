da205ced7ddcd39b3937611e1a55d59ac619b3ed
Dominik Haas <shaodiese@gmail.com>
2020-08-20 11:05:02 +0200

    added the useTestdata to the resolving of the actionUrl

--- src/modules/user/store/userActions.js
+++ src/modules/user/store/userActions.js
@@ -5,7 +5,7 @@
  * @author Dominik Haas-Artho
  *
  * Created at     : 2020-07-14 16:51:52
- * Last modified  : 2020-08-19 16:43:12
+ * Last modified  : 2020-08-20 11:02:11
  *
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE.txt', which is part of this source code package.
@@ -19,29 +19,52 @@ import {
   FETCH_USER_DATA,
 } from './userMutationsConsts';
 
-// don't have an api base url or proxy to be able to load local testdata
-const API_BASE = process.env.NODE_ENV === 'development' ? '' : '/api/action/';
-const ENVIDAT_PROXY = process.env.NODE_ENV === 'development' ? '' : process.env.VUE_APP_ENVIDAT_PROXY;
+// don't use an api base url or proxy when using testdata
+let API_BASE = '';
+let ENVIDAT_PROXY = '';
 
+const useTestdata = process.env.VUE_APP_USE_TESTDATA === 'true';
+
+if (!useTestdata) {
+  API_BASE = '/api/action/';
+  ENVIDAT_PROXY = process.env.VUE_APP_ENVIDAT_PROXY;  
+}
+
+function extractBodyIntoUrl(url, body) {
+  const keys = Object.keys(body);
+
+  if (keys && keys.length > 0) {
+    url += '?';
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      if (i > 0) {
+        url += '&';
+      }
+      url += `${key}=${body[key]}`;
+    }
+  }
+
+  return url;
+}
 
 export default {
   async [FETCH_USER_DATA]({ commit }, payload) {
     commit(payload.mutation);
 
-    let body = { };
-
-    if (payload) {
-      body = payload.body;
-    }
+    const body = payload.body || {};
 
     // unpack the action because it might be wrapped to provide a test url
-    const actionUrl = typeof (payload.action) === 'function' ? payload.action() : payload.action;
+    let actionUrl = typeof (payload.action) === 'function' ? payload.action() : payload.action;
+
+    actionUrl = extractBodyIntoUrl(actionUrl, body);
+
     const url = urlRewrite(actionUrl, API_BASE, ENVIDAT_PROXY);
 
     // if the url is directly to a file it has to be a get call
-    const method = url.includes('.json') ? 'get' : 'post';
+    // const method = url.includes('.json') ? 'get' : 'post';
 
-    await axios({ method, url, body })
+    await axios.get(url)
+    // await axios({ method, url, body })
     .then((response) => {
         if (payload.commit) {
           commit(`${payload.mutation}_SUCCESS`, response.data.result);
--- src/modules/user/store/userMutationsConsts.js
+++ src/modules/user/store/userMutationsConsts.js
@@ -3,17 +3,19 @@
  * @author Dominik Haas-Artho
  *
  * Created at     : 2020-07-14 16:38:28
- * Last modified  : 2020-08-19 16:34:44
+ * Last modified  : 2020-08-20 10:57:24
  *
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE.txt', which is part of this source code package.
  */
 
+const useTestdata = process.env.VUE_APP_USE_TESTDATA === 'true';
+
 export const GET_USER_CONTEXT = 'GET_USER_CONTEXT';
 export const GET_USER_CONTEXT_ERROR = 'GET_USER_CONTEXT_ERROR';
 export const GET_USER_CONTEXT_SUCCESS = 'GET_USER_CONTEXT_SUCCESS';
 export const ACTION_GET_USER_CONTEXT = () => {
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development' && useTestdata) {
     return './testdata/envidat_context_user_show.json';
   }
 
@@ -24,7 +26,7 @@ export const USER_SIGNIN = 'USER_SIGNIN';
 export const USER_SIGNIN_ERROR = 'USER_SIGNIN_ERROR';
 export const USER_SIGNIN_SUCCESS = 'USER_SIGNIN_SUCCESS';
 export const ACTION_USER_SIGNIN = () => {
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development' && useTestdata) {
     return './testdata/passwordless_user_login.json';
   }
 
@@ -35,7 +37,7 @@ export const REQUEST_TOKEN = 'REQUEST_TOKEN';
 export const REQUEST_TOKEN_ERROR = 'REQUEST_TOKEN_ERROR';
 export const REQUEST_TOKEN_SUCCESS = 'REQUEST_TOKEN_SUCCESS';
 export const ACTION_REQUEST_TOKEN = () => {
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development' && useTestdata) {
     return './testdata/passwordless_perform_reset.json';
   }
 
@@ -46,7 +48,7 @@ export const USER_SIGNOUT = 'USER_SIGNOUT';
 export const USER_SIGNOUT_ERROR = 'USER_SIGNOUT_ERROR';
 export const USER_SIGNOUT_SUCCESS = 'USER_SIGNOUT_SUCCESS';
 export const ACTION_USER_SIGNOUT = () => {
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development' && useTestdata) {
     return './testdata/passwordless_user_logout.json';
   }
 
@@ -60,7 +62,7 @@ export const NOT_AUTHORIZED_ERROR = 'NotAuthorized';
 export const FETCH_USER_DATA = 'FETCH_USER_DATA';
   
 export const ACTION_USER_SHOW = () => {
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development' && useTestdata) {
     return './testdata/user_show.json';
   }
 
