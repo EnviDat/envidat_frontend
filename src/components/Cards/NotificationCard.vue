f814d36f820df5cf79a1830310f66ac85f8a05f7
Dominik <shaodiese@gmail.com>
2020-08-27 17:19:57 +0200

    fixed layout on notification card;

--- src/components/Cards/NotificationCard.vue
+++ src/components/Cards/NotificationCard.vue
@@ -1,39 +1,40 @@
 <template>
   <v-card height="165"
-          class="elevation-5"
+          class="elevation-3"
           :color="notification.color">
 
-    <v-card-title class="pb-0">
-      <v-container class="pa-0" >
-        <v-row align-content="center">
+    <v-card-title >
+      <v-row align-content="center" no-gutters>
 
-          <v-col cols="1" >
-            <v-icon>{{ notification.icon }}</v-icon>
-          </v-col>
+        <v-col cols="1" >
+          <v-icon>{{ notification.icon }}</v-icon>
+        </v-col>
 
-          <v-col cols="11">
-            <div class="title">{{ notification.message }}</div>
-          </v-col>
+        <v-col cols="11">
+          <div class="title">{{ notification.message }}</div>
+        </v-col>
 
-          <v-col cols="12">
-            <div style="height: 60px; overflow: hidden scroll;"
-                class="caption"> {{ notification.details }}</div>
-          </v-col>
-
-        </v-row>
-      </v-container>
+      </v-row>
     </v-card-title>
 
-    <v-card-text class="px-2 py-1">
-      <v-row class="fill-height"  align="end" justify="end">
-        <v-col class="shrink" v-if="showReportButton"
-                >
+    <v-card-text v-html="notification.details">
+      {{ notification.details }}
+    </v-card-text>
+
+    <v-card-actions class="ma-0 py-0 px-2"
+                    style="position: absolute; bottom: 0px; right: 5px;" >
+      <v-row class="fill-height" 
+              align="end"
+              justify="end">
+        <v-col v-if="showReportButton"
+                class="shrink" >
           <base-rectangle-button buttonText="Report"
                                   isSmall
                                   @clicked="$emit('clickedReport')" />
         </v-col>
 
-        <v-col class="shrink" >
+        <v-col v-if="showCloseButton"
+                class="shrink" >
           <base-rectangle-button color="black"
                                   buttonText="Close"
                                   isSmall
@@ -41,7 +42,7 @@
                                   @clicked="$emit('clickedClose')" />
         </v-col>
       </v-row>
-    </v-card-text>
+    </v-card-actions>
 
   </v-card>
 </template>
@@ -63,9 +64,6 @@
 
 import BaseRectangleButton from '@/components/BaseElements/BaseRectangleButton';
 
-// checkout skeleton
-// https://github.com/ToxicJojo/SkeletonPlaceholder
-
 export default {
   components: {
     BaseRectangleButton,
@@ -73,9 +71,15 @@ export default {
   props: {
     notification: Object,
     showReportButton: Boolean,
+    showCloseButton: {
+      type: Boolean,
+      default: true,
+    },
   },
   mounted() {
-    this.setTimeout();
+    if (this.notification.timeout > 0) {
+      this.setTimeout();
+    }
   },
   computed: {
   },
