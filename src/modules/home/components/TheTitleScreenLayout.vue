c6bc6dd9442cff7d7c163a52b22e8c1bb92383f3
Dominik Haas <shaodiese@gmail.com>
2020-10-15 18:27:56 +0200

    .6.91: Auto stash before merge of "release/vuetify_upgrade_0.6.91" and "origin/release/vuetify_upgrade_0.6.91"

--- src/modules/home/components/TheTitleScreenLayout.vue
+++ src/modules/home/components/TheTitleScreenLayout.vue
@@ -62,7 +62,7 @@
  * @author Dominik Haas-Artho
  *
  * Created at     : 2019-10-23 14:11:27
- * Last modified  : 2019-10-31 08:20:02
+ * Last modified  : 2020-10-13 22:10:44
  *
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE.txt', which is part of this source code package.
