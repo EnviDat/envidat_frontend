0f2ddcc4eca2bec816403dda8b6a235a4163d021
Dominik <shaodiese@gmail.com>
2020-10-15 18:04:45 +0200

    added backend call to resolve the related publications
    with axios and regex resolving for the ids, need refactoring

--- src/modules/metadata/components/Metadata/MetadataPublications.vue
+++ src/modules/metadata/components/Metadata/MetadataPublications.vue
@@ -1,8 +1,10 @@
 <template  id="MetadataPublications">
-  <expandable-text-layout v-bind="publications"
-                            :showPlaceholder="showPlaceholder"
-                            :maxTextLength="publications.maxTextLength"
-                            :emptyTextColor="publications.emptyTextColor" />
+  <expandable-text-layout :title="publications.title"
+                          :text="publicationsText"
+                          :showPlaceholder="showPlaceholder || isResolving"
+                          :maxTextLength="publications ? publications.maxTextLength : undefined"
+                          :emptyTextColor="publications ? publications.emptyTextColor : undefined"
+                          class="relatedPubList"/>
 </template>
 
 <script>
@@ -19,6 +21,7 @@
  * file 'LICENSE.txt', which is part of this source code package.
 */
 
+import axios from 'axios';
 import ExpandableTextLayout from '@/components/Layouts/ExpandableTextLayout';
 
 export default {
@@ -30,14 +33,134 @@ export default {
     genericProps: Object,
     showPlaceholder: Boolean,
   },
-  computed: {
+  computed: {    
     publications() {
-      return this.mixinMethods_getGenericProp('publications');
+      return this.mixinMethods_getGenericProp('publications', {
+        title: 'Related Publications',
+        text: '',
+      });
+    },
+    publicationsConfig() {
+      const metaConfig = this.mixinMethods_getGenericProp('metadataConfig');
+      return metaConfig?.publicationsConfig || {};
+    },
+    publicationsText() {
+      if (this.resolveDone && this.resolvedPublications) {
+        let publicationsText = this.publications.text;
+
+        this.idsToResolve.forEach((id) => {
+          const resolvedObject = this.resolvedPublications[id];
+          const text = resolvedObject?.citation?.ACS;
+          if (text) {
+            publicationsText = publicationsText.replace(id, text);
+          }
+        });
+
+        return publicationsText;
+      }
+
+      return this.publications?.text;
+    },
+    resolveIdsActive() {
+      return this.resolveIds && this.resolveBaseUrl;
+    },
+    resolveIds() {
+      return this.publicationsConfig.resolveIds;
+    },
+    idDelimiter() {
+      return this.publicationsConfig.idDelimiter;
+    },
+    idPrefix() {
+      return this.publicationsConfig.idPrefix;
+    },
+    resolveBaseUrl() {
+      return this.publicationsConfig.resolveBaseUrl;
+    },
+    textContainsIds() {
+      return this.idsToResolve && this.idsToResolve.length > 0 && this.idsToResolve[0] !== '';      
     },
   },
   methods: {
+    extractTextIds(idString) {
+      if (idString) {
+        const delimiter = this.idDelimiter || '';
+        const prefix = this.idPrefix || '';
+
+        const regExStr = `\\${prefix}\\s?[a-zA-Z]+${delimiter}\\d+`;
+        const regEx = new RegExp(regExStr, 'gm');
+        const hasValidIds = idString.match(regEx);
+        // console.log(`hasValidIds ${hasValidIds?.length}`);
+
+        const ids = [];
+        hasValidIds.forEach((match) => {
+          let idOnly = match;
+          if (prefix) {
+            idOnly = idOnly.replace(prefix, '');
+          }
+
+          ids.push(idOnly.trim());
+          // console.log(`Found match, group ${groupIndex}: ${match}`);
+        });        
+
+        return ids;
+      }
+
+      return [];
+    },
+    async resolvePublications() {
+      const requests = [];
+      this.isResolving = true;
+
+      this.idsToResolve.forEach((id) => {
+        
+        const url = this.resolveBaseUrl + id;
+
+        requests.push(axios.get(url));
+      });
+
+      await Promise.all(requests)
+        .then((responses) => {
+          this.isResolving = false;
+          for (let i = 0; i < responses.length; i++) {
+            const response = responses[i];
+            this.resolvedPublications = { ...this.resolvedPublications, ...response.data };
+          }
+
+          this.resolveDone = true;          
+        })
+        .catch((error) => {
+          this.isResolving = false;
+          this.resolveDone = true;          
+          console.log(`publications resolving error: ${error}`);
+        });
+    },
+  },
+  updated() {
+//     this.textContainsIds(`* wsl:21234
+// * psi:24720`);
+    if (this.resolveIdsActive && !this.isResolving && !this.resolveDone) {
+
+      this.idsToResolve = this.extractTextIds(this.publications.text);
+
+      if (this.textContainsIds) {
+        this.resolvePublications();
+      }
+    }
+
+    // if (this.resolveDone && this.isResolving) {
+    //   this.re
+    // }
   },
   data: () => ({
+    resolveDone: false,
+    isResolving: false,
+    idsToResolve: [],
+    resolvedPublications: null,
   }),
 };
 </script>
+
+<style lang="sass">
+  .relatedPubList .readableText ul > * + *
+    padding: 5px 0px
+</style>
