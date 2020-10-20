e399d5b58b700bf3204c60002e4719083557b977
Dominik Haas <shaodiese@gmail.com>
2020-10-20 10:37:59 +0200

    added constants for metadata detail strings;
    adjusted the default definitions for metadata detail components to be defined by the components not in the factory;

---
+++ src/factories/metadataConsts.js
@@ -0,0 +1,26 @@
+/**
+ * constants for any metadata context
+ *
+ * @summary consts for metadata
+ * @author Dominik Haas-Artho
+ *
+ * Created at     : 2020-10-15 18:34:51
+ * Last modified  : 2020-10-15 19:15:56
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+
+export const METADATA_BODY_TITLE = 'Description';
+export const METADATA_CITATION_TITLE = 'Citation';
+export const METADATA_PUBLICATIONS_TITLE = 'Related Publications';
+
+export const METADATA_AUTHORS_TITLE = 'Author Details';
+export const AUTHORS_PUBLISHED_DATACOUNT = 'Published datasets';
+export const AUTHORS_DATACREDIT_CONTRIBUTION = 'Data Credit Contributions';
+export const AUTHORS_DATACREDIT_SCORE = 'Data Credit Score';
+
+export const METADATA_DETAILS_TITLE = 'Further Information';
+export const METADATA_FUNDING_TITLE = 'Funding Information';
+export const METADATA_LOCATION_TITLE = 'Location';
+export const METADATA_RESOURCES_TITLE = 'Data and resources';
