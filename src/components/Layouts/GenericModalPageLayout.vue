0b1b0cc1710433a643649eb4797b21444bfd7555
Dominik Haas <shaodiese@gmail.com>
2021-01-19 16:04:59 +0100

    added GenericModalPageLayout

---
+++ src/components/Layouts/GenericModalPageLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-dialog v-model="dialog"
+            fullscreen
+            hide-overlay
+            transition="dialog-bottom-transition"
+            scrollable >
+
+    <slot name="filterKeywords" />
+
+  </v-dialog>
+</template>
+
+<script>
+/**
+ * GenericModalPageLayout.vue renders a list of markdown objects
+ *
+ * @summary renders a list of markdown objects
+ * @author Dominik Haas-Artho
+ *
+ * Created at     : 2019-10-23 14:11:27
+ * Last modified  : 2019-10-23 14:54:16
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+*/
+
+export default {
+  components: {
+  },
+  props: {
+  },
+  methods: {
+  },
+  data: () => ({
+    dialog: false,
+  }),
+};
+</script>
