042a09b1c91798bceacb8b16380cf5d4ed204c78
Dominik Haas <shaodiese@gmail.com>
2021-02-02 15:54:23 +0100

    comment for rebecca

--- src/modules/metadata/components/MetadataDetailPage.vue
+++ src/modules/metadata/components/MetadataDetailPage.vue
@@ -82,7 +82,7 @@
  * @author Dominik Haas-Artho
  *
  * Created at     : 2019-10-23 16:12:30
- * Last modified  : 2021-02-02 14:54:40
+ * Last modified  : 2021-02-02 15:50:07
  *
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE.txt', which is part of this source code package.
@@ -332,6 +332,10 @@ export default {
       .get(url)
       .then((response) => {
         this.stationsConfig = response.data;
+
+        // Rebecca:
+        // get now date, substract 14days
+        // go through each station add the timestamps to the url
 
         this.injectMicroCharts();
       })
